c1e353fb1d58c4ed3d8f9dac2af9fdcc53935009
Ivan Villacorta <ivanvillacorta@Mac-Ivan.local>
2021-06-01 19:07:27 -0300

    ajustes

--- src/app/pages/usuarios/usuarios.component.ts
+++ src/app/pages/usuarios/usuarios.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuariosComponent implements OnInit {
 
+  usuarios = [
+    {
+      id: 1,
+      nome: 'Ivan Villacorta',
+      email: 'ivan_mv23@hotmai.com',
+      cpf: '39293316838'
+    },
+    {
+      id: 2,
+      nome: 'Adalberto Torres',
+      email: 'adal@hgmail.com',
+      cpf: '39293316839'
+    },
+    {
+      id: 3,
+      nome: 'Maria da Silva',
+      email: 'maria_silva@hotmai.com',
+      cpf: '39293316840'
+    }
+  ];
+
   constructor(
     
   ) {
